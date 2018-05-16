ec03813119ece17f1d52b3bc931e4e81f93121bd
PramuPerera <pramuditha@live.com>
2018-05-16 00:13:45 -0400

    Create genDATANIR.py

---
+++ genDATANIR.py
@@ -0,0 +1,33 @@
+import glob
+from scipy.misc import imread 
+from scipy.misc import imsave
+from scipy.misc import toimage
+from scipy.misc import imresize
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+
+import os
+datasetA ='/home/labuser/Documents/data/nirscene/*nir.tiff'
+datasetB  = '/home/labuser/Documents/data/nirscene/*rgb.tiff'
+destination = 'datasets/NIRtoVIS/'
+dataA = glob.glob(datasetA)
+dataA = sorted(dataA)
+dataB = glob.glob(datasetB)
+dataB = sorted(dataB)
+count = 0
+for i,j in zip(dataA,dataB):
+	count+=1
+	img_A = imread(j)
+	img_B = imread(i,mode="L")
+	line = img_A[:,:,1]
+	out = np.zeros((np.shape(img_B)[0],np.shape(img_B)[1]*2,3))
+	out[:,0:np.shape(img_B)[1],0] = img_B
+	out[:,0:np.shape(img_B)[1],1] = img_B
+	out[:,0:np.shape(img_B)[1],2] = img_B
+	out[:,np.shape(img_B)[1]:2*np.shape(img_B)[1],0] = line
+	out[:,np.shape(img_B)[1]:2*np.shape(img_B)[1],1] = line
+	out[:,np.shape(img_B)[1]:2*np.shape(img_B)[1],2] = line
+	toimage(out, cmin=0, cmax=255).save(destination+'trainA/'+str(count)+'.jpg')
+	toimage(img_A, cmin=0, cmax=255).save(destination+'trainB/'+str(count)+'.jpg')
